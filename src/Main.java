e470ddec87a16b3f2285a5ea7ab79fd6122165a3
llfflelf <dlehdtn48@daegu.ac.kr>
2025-06-05 10:33:10 +0900

    third commit(x6)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        for(int i = 1; i <=20; i++){
-            if (i > 15) {
-                break;
-            }
-            if(i % 5 == 0){
+        for(int i = 1; i <=1000; i++){
+            if(i % 2 != 0 || i % 3 != 0){
+                if(i == 100){
+                    break;
+                }
                 continue;
             }
             System.out.println(i);
